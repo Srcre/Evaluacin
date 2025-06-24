56fdf3c608cd8ac799f10ba0160ca3efbc88c711
Srcre <nicolashernandeznh0234@gmail.com>
2025-06-23 20:14:13 -0400

    Update app.py
    cambios pocos

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ def create_app():
     login_manager.login_message_category = "warning"
 
     # Tu contraseña con caracteres especiales
-    my_password = '49^!M&*TC2*g!tb*pR@c!x8@localhost'
+    my_password = 'admin'
 
     # Codifica la contraseña para URL
     ncoded_password = urllib.parse.quote_plus(my_password)
 
     # Configura la cadena de conexión con la contraseña codificada
     # Asegúrate de que el host sea 'localhost' o '127.0.0.1' y el nombre de tu base de datos
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{ncoded_password}@localhost:3306/nombre_de_tu_base_de_datos'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://root:{ncoded_password}@localhost:3306/mi_app_db'
 
     # Ejemplo: Imprime la URI para verificarla
     print(f"SQLALCHEMY_DATABASE_URI: {app.config['SQLALCHEMY_DATABASE_URI']}")
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     # Creación de tablas de la base de datos (solo una vez)
     # Se recomienda ejecutar esto en un shell de Python o en un script de migración
     with app.app_context():
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:7@49^!M&*TC2*g!tb*pR@c!x8@localhost/mi_app_db'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin@localhost/mi_app_db'
         print("Tablas de la base de datos creadas (si no existían).")
 
     app.run(debug=True) # debug=True para desarrollo, cambiar a False en producción
